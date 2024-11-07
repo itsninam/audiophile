e10dad2fad972ed5d4d6944134c90e8a77485e5e
Madalina <madalinaoancea@hotmail.com>
2024-11-06 21:08:45 -0500

    Cart modal rough work

--- src/components/navigation/Cart.tsx
+++ src/components/navigation/Cart.tsx
@@ -1,16 +1,46 @@
+import { useEffect, useState } from "react";
 import { useProduct } from "../../contexts/ProductContext";
 import styles from "./Nav.module.scss";
+import CartModal from "./CartModal";
 
 function Cart() {
   const { cartItems } = useProduct();
+  const [showCartModal, setShowCartModal] = useState(false);
+  const cartItemsTotal = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", (event) => {
+      if ((event.target as Element).id === "cart-modal") {
+        setShowCartModal(false);
+      }
+    });
+
+    if (showCartModal) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [showCartModal]);
+
   return (
-    <div className={styles.cartContainer}>
-      <img src="/starter-code/assets/shared/desktop/icon-cart.svg" alt="cart" />
-      <p className={styles.cartItems}>
-        {cartItems.length > 0 &&
-          cartItems.map((item) => item.quantity).reduce((a, b) => a + b, 0)}
-      </p>
-    </div>
+    <>
+      <div
+        className={styles.cartContainer}
+        onClick={() => setShowCartModal(!showCartModal)}
+      >
+        <img
+          src="/starter-code/assets/shared/desktop/icon-cart.svg"
+          alt="cart"
+        />
+        <p className={styles.cartItems}>
+          {cartItems.length > 0 && cartItemsTotal}
+        </p>
+      </div>
+      {showCartModal && <CartModal cartItemsTotal={cartItemsTotal} />}
+    </>
   );
 }
 
--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 import Cart from "./Cart";
 import NavList from "./NavList";
 
-function NavBar({ isFooterNav }: { isFooterNav?: boolean }) {
+function NavBar() {
   return (
     <nav className={styles.nav}>
       <div className={`${styles.wrapper} wrapper`}>
